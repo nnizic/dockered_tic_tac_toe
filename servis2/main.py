77a9a960186505ed74047630ea022acec5fee3ef
giz73 <nneven@proton.m>
2025-02-26 20:51:54 +0100

    poboljšana logika igrača

--- servis2/main.py
+++ servis2/main.py
@@ -1,25 +1,44 @@
 """Servis 2 (Igrač X)"""
 
+import random
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+SIMBOL = "X"
 
 
 class PoljeModel(BaseModel):
     polje: list
 
 
-@app.post("/odigraj")
-async def odigraj_potez(podaci: PoljeModel):
-    """Igrač X bira prvo slobodno mjesto"""
-    polje = podaci.polje
-
+def odigraj_na_slobodno(polje):
+    """postavlja X u prvo prazno polje"""
     for i in range(3):
         for j in range(3):
             if polje[i][j] == " ":
-                polje[i][j] = "X"
+                polje[i][j] = SIMBOL
 
-                return {"polje": polje}
+                return polje
+    return polje
+
+
+def odigraj_random(polje):
+    """postavlja X u nasumično polje"""
+    slobodna_polja = [(i, j) for i in range(3) for j in range(3) if polje[i][j] == " "]
+    if slobodna_polja:
+        i, j = random.choice(slobodna_polja)
+        polje[i][j] = SIMBOL
+    return polje
+
+
+@app.post("/odigraj")
+async def odigraj_potez(podaci: PoljeModel):
+    """Prima trenutno stanje polja i vraća novo stanje ( nakon poteza )"""
+    polje = podaci.polje
+    strategija = random.choice([odigraj_na_slobodno, odigraj_random])
+    polje = strategija(polje)
 
     return {"polje": polje}
--- servis3/main.py
+++ servis3/main.py
@@ -7,19 +7,38 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+SIMBOL = "O"
+
 
 class PoljeModel(BaseModel):
     polje: list
 
 
+def odigraj_na_slobodno(polje):
+    """postavlja O u prvo prazno polje"""
+    for i in range(3):
+        for j in range(3):
+            if polje[i][j] == " ":
+                polje[i][j] = SIMBOL
+
+                return polje
+    return polje
+
+
+def odigraj_random(polje):
+    """postavlja O u nasumično polje"""
+    slobodna_polja = [(i, j) for i in range(3) for j in range(3) if polje[i][j] == " "]
+    if slobodna_polja:
+        i, j = random.choice(slobodna_polja)
+        polje[i][j] = SIMBOL
+    return polje
+
+
 @app.post("/odigraj")
 async def odigraj_potez(podaci: PoljeModel):
-    """Igrač O bira nasumično slobodno mjesto."""
+    """Prima trenutno stanje polja i vraća novo stanje ( nakon poteza )"""
     polje = podaci.polje
-    slobodna_mjesta = [(i, j) for i in range(3) for j in range(3) if polje[i][j] == " "]
-
-    if slobodna_mjesta:
-        i, j = random.choice(slobodna_mjesta)
-        polje[i][j] = "O"
+    strategija = random.choice([odigraj_na_slobodno, odigraj_random])
+    polje = strategija(polje)
 
     return {"polje": polje}
