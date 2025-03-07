2a6523ccdd65d2dad77e3f587d4490f46e915d11
giz73 <nneven@proton.m>
2025-03-07 22:03:37 +0100

    poboljšana logika igrača X

--- servis2/main.py
+++ servis2/main.py
@@ -25,13 +25,24 @@ def odigraj_na_slobodno(polje):
     return polje
 
 
-def odigraj_random(polje):
-    """postavlja X u nasumično polje"""
+def odigraj_optimalno(polje):
+    """prvo bira sredinu, onda kutove, onda random"""
+    sredina = (1, 1)
+    if polje[sredina[0]][sredina[1]] == " ":
+        polje[sredina[0]][sredina[1]] = SIMBOL
+        return polje
+    kutovi = [(0, 0), (0, 2), (2, 0), (2, 2)]
+    slobodni_kutovi = [pos for pos in kutovi if polje[pos[0]][pos[1]] == " "]
+    if slobodni_kutovi:
+        slobodni_kut = random.choice(slobodni_kutovi)
+        polje[slobodni_kut[0]][slobodni_kut[1]] = SIMBOL
+        return polje
+
     slobodna_polja = [(i, j) for i in range(3) for j in range(3) if polje[i][j] == " "]
     if slobodna_polja:
         i, j = random.choice(slobodna_polja)
         polje[i][j] = SIMBOL
-    return polje
+        return polje
 
 
 @app.post("/odigraj")
@@ -41,7 +52,7 @@ async def odigraj_potez(podaci: PoljeModel):
         raise HTTPException(status_code=500, detail="Greška u servisu")
 
     polje = podaci.polje
-    strategija = random.choice([odigraj_na_slobodno, odigraj_random])
+    strategija = random.choice([odigraj_na_slobodno, odigraj_optimalno])
     polje = strategija(polje)
 
     return {"polje": polje}
